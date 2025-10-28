35a8da941ea9a2f2ef2c377a23f071ae45e6f49a
devopsbyvasantha <devopsbyvasantha@gmail.com>
2025-10-28 13:16:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.medium"
-  key_name               = "Netflix"
+  key_name               = "vkeypair"
   vpc_security_group_ids = ["sg-00f3f3e5bc8d7a27a"]
   tags = {
     Name = var.instance_names[count.index]
