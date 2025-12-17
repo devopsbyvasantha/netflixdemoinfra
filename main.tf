a8aa7b8bcb13441ea95903c84122d6d3e106acb4
devopsbyvasantha <devopsbyvasantha@gmail.com>
2025-12-17 14:20:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-061fe7df6ad657197"
   instance_type          = "t2.medium"
-  key_name               = "jenkins"
-  vpc_security_group_ids = ["sg-00c549897f85df846"]
+  key_name               = "newkeypair"
+  vpc_security_group_ids = ["sg-0fb2de3f44f20e2e2"]
   tags = {
     Name = var.instance_names[count.index]
   }
